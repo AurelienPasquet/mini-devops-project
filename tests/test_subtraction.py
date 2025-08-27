91ebc60a1f961087837b7a316a4c9455bce55961
AurelienPasquet <apasquet@insa-toulouse.fr>
2025-08-27 19:29:43 +0200

    Fixed typo

--- tests/test_subtraction.py
+++ tests/test_subtraction.py
@@ -5,6 +5,6 @@ from substraction_service.substraction_service import app
 client = TestClient(app)
 
 def test_substraction():
-    response = client.post("/add", json={"a": 10, "b": 4})
+    response = client.post("/sub", json={"a": 10, "b": 4})
     assert response.status_code == 200
     assert response.json() == {"result": 6}
